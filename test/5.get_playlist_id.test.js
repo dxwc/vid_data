ecc3d6fdcd53704cc52fbc7a56d5ba0a85d64e5b
dxwc <dxwc@users.noreply.github.com>
2019-01-03 01:16:58 -0500

    add get_playlist_id tests

---
+++ test/5.get_playlist_id.test.js
@@ -0,0 +1,50 @@
+const assert = require('assert');
+const faker  = require('faker');
+
+const vd = require('../index.js');
+
+describe('get_playlist_id() test', () =>
+{
+    it('should return null on invalid input', () =>
+    {
+        assert(vd.get_playlist_id() === null);
+        assert(vd.get_playlist_id('') === null);
+        assert(vd.get_playlist_id(' ') === null);
+        assert(vd.get_playlist_id(faker.internet.url()) === null);
+        assert
+        (
+            vd.get_playlist_id(`https://www.youtube.com/watch?v=Y4ETyLbjQ6c&t=159`)
+            === null
+        );
+    });
+
+    it('should return playlist id on valid input', () =>
+    {
+        assert
+        (
+            vd.get_playlist_id
+            (
+                `https://www.youtube.com/playlist?list=` +
+                `PLcviVtB85dLwp3cUw85eBJEPK8SwpNMBC`
+            ) === `PLcviVtB85dLwp3cUw85eBJEPK8SwpNMBC`
+        );
+
+        assert
+        (
+            vd.get_playlist_id
+            (
+                `https://www.youtube.com/watch?v=o9qRjzve8Oc&list=` +
+                `PLcviVtB85dLwp3cUw85eBJEPK8SwpNMBC`
+            ) === `PLcviVtB85dLwp3cUw85eBJEPK8SwpNMBC`
+        );
+
+        assert
+        (
+            vd.get_playlist_id
+            (
+                `https://www.youtube.com/watch?v=WqwsvKi5FRE&list` +
+                `=PLcviVtB85dLwp3cUw85eBJEPK8SwpNMBC&index=8&t=0s`
+            ) === `PLcviVtB85dLwp3cUw85eBJEPK8SwpNMBC`
+        );
+    });
+});
